c78d301955970cea8c19d7b7213abbf69d08258d
Niall McCafferty <s00165352@mail.itsligo.ie>
2018-02-16 15:43:45 +0000

    Sign in

--- js/main.js
+++ js/main.js
@@ -8,12 +8,19 @@ const FIREBASE_AUTH = firebase.auth();
 const FIREBASE_MESSAGING = firebase.messaging();
 const FIREBASE_DATABASE = firebase.database();
 
+const signInButton = document.getElementById('sign-in');
+
 /* ========================
   Event Listeners
 ======================== */
+signInButton.addEventListener('click', signIn);
 
 /* ========================
   Functions
 ======================== */
+
+function signIn(){
+  FIREBASE_AUTH.signIn( new firebase.auth.GoogleAuthProvider());
+}
 
 }
